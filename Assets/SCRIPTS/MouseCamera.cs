d699a3c3faae3626efe30b9571219795172d85a4
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-10-21 23:40:26 -0300

    grass

---
+++ Assets/SCRIPTS/MouseCamera.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamera : MonoBehaviour
+{
+    public Vector2 turn;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0); 
+    }
+}
