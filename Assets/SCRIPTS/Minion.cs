bc18d1b9f16d7bc8d9f8fb3321b9aabfa108a8f5
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-10-22 13:45:03 -0300

    Mob movement WIP

--- Assets/SCRIPTS/Minion.cs
+++ Assets/SCRIPTS/Minion.cs
@@ -1,5 +1,6 @@
 
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Minion : MonoBehaviour
@@ -7,7 +8,11 @@ public class Minion : MonoBehaviour
     public Minion target; // TODO AUTOTARGETING. Enemy nexus must be allways the last target in the list
     public Action OnDeath;
     public int health = 100;
-    bool isMob;
+    public bool moveAsMob;
+
+    [SerializeField] Vector3 mobDesiredPosition;
+    [SerializeField] int mobMovementCountdown;
+
 
     // TODO Scrapear Lifeform de DreamQuest2Prototype (Y todo lo demas)
     void Update()
@@ -15,28 +20,23 @@ public class Minion : MonoBehaviour
         if (health <= 0)
             OnDeath?.Invoke();
 
-        if (target != null)
+        if (!moveAsMob && target != null)
             transform.position = Vector3.MoveTowards(transform.position, target.transform.position, .03f);
+        else if (moveAsMob) // reset countdown & decide new position
+        {
+            if (mobMovementCountdown > 0) 
+                mobMovementCountdown--;
+            else 
+                mobMovementCountdown = 100;
 
-        if (isMob)
-            MobMovement();
+            var randomNewPosition = transform.position + new Vector3(UnityEngine.Random.Range(-5, 5), 0, UnityEngine.Random.Range(-5, 5));
+            randomNewPosition.y = Terrain.activeTerrain.SampleHeight(randomNewPosition);
+            transform.position = Vector3.MoveTowards(transform.position, randomNewPosition, .03f);
+        }
     }
 
     public void SetTarget(Minion target)
     {
         this.target = target;
-    }
-
-    public void IsMob(bool spawnPosition)
-    {
-        isMob = true;
-    }
-
-    public void MobMovement()
-    {
-        var randomNewPosition = transform.position + new Vector3(UnityEngine.Random.Range(-5, 5), 0, UnityEngine.Random.Range(-5, 5));
-        randomNewPosition.y = Terrain.activeTerrain.SampleHeight(randomNewPosition);
-
-        transform.position = Vector3.MoveTowards(transform.position, randomNewPosition, .03f);
     }
 }
