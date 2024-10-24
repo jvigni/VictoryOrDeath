55aecec2fe644883ddbde072c8d688a120988d23
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-10-24 15:19:51 -0300

    Undead minions

--- Assets/MINIONS/Minion.cs
+++ Assets/MINIONS/Minion.cs
@@ -7,15 +7,19 @@ public class Minion : MonoBehaviour
 {
     public LifeForm Target;
     [SerializeField] NexusSpawner nexusToOBLITERATE;
-    
+    [SerializeField] float speed = 5f;
+    Vector3 velocity;
+
     void Update()
     {
-        if (Target != null) 
+        if (Target != null)
         {
             // TODO
-        } else
+        }
+        else
             transform.position = Vector3.MoveTowards(transform.position, nexusToOBLITERATE.transform.position, .03f);
-        
+            //velocity = new Vector3(speed, 0, 0);
+            //transform.position = Vector3.SmoothDamp(transform.position, nexusToOBLITERATE.transform.position, ref velocity, 03f);
     }
 
     public void SetNexusToOBLITERATE(NexusSpawner shittyNexus)
