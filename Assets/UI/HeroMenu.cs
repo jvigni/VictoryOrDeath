ab65c6fde8081a1413761f17aef931b64923f7ce
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-10-25 07:24:12 -0300

    UI coode

--- Assets/UI/HeroMenu.cs
+++ Assets/UI/HeroMenu.cs
@@ -1,8 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HeroMenu : MonoBehaviour
 {
-    public Image Image;
+    public EquipmentSlot head;
+    public EquipmentSlot cloak;
+    public EquipmentSlot armor;
+    public EquipmentSlot relic;
+    public EquipmentSlot gloves;
+    public EquipmentSlot boots;
+    public EquipmentSlot main_hand;
+    public EquipmentSlot off_hand;
+
+    public string class_name;
+    public int power_level;
+    public Image element1Img;
+    public Image element2Img;
+    public string element1txt;
+    public string element2txt;
+
+    public StatSlot damageStat;
+    public StatSlot defenseStat;
+    public StatSlot staminaStat;
+    public StatSlot manaStat;
+
+    public List<InventorySlot> inventory;
+    public GameObject trash;
+
+    public int resources;
+    public List<UpgradeSlot> Upgrades;
 }
--- Assets/UI/InventorySlot.cs
+++ Assets/UI/InventorySlot.cs
@@ -4,14 +4,7 @@ using UnityEngine;
 
 public class InventorySlot : MonoBehaviour
 {
-    [SerializeField] EquipmentSlot head;
-    [SerializeField] EquipmentSlot cloak;
-    [SerializeField] EquipmentSlot armor;
-    [SerializeField] EquipmentSlot relic;
-    [SerializeField] EquipmentSlot gloves;
-    [SerializeField] EquipmentSlot boots;
-    [SerializeField] EquipmentSlot main_hand;
-    [SerializeField] EquipmentSlot off_hand;
+    [SerializeField] List<InventorySlot> inventorySlots;
 
 
 }
