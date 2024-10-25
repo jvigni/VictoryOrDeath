ee5af1bf0304daf15666d1d187781366c6823310
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-10-25 17:59:19 -0300

    gold ore variants

--- Assets/GOLD_ORE/GoldOre.cs
+++ Assets/GOLD_ORE/GoldOre.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoldRT : MonoBehaviour
+public class GoldOre : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
