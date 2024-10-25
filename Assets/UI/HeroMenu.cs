dfb9f5762efe4466aed095c8d5db3f6b16a2af3e
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-10-25 07:10:23 -0300

    Scripting UI

---
+++ Assets/UI/HeroMenu.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroMenu : MonoBehaviour
+{
+    public Image Image;
+}
---
+++ Assets/UI/InventorySlot.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventorySlot : MonoBehaviour
+{
+    [SerializeField] EquipmentSlot head;
+    [SerializeField] EquipmentSlot cloak;
+    [SerializeField] EquipmentSlot armor;
+    [SerializeField] EquipmentSlot relic;
+    [SerializeField] EquipmentSlot gloves;
+    [SerializeField] EquipmentSlot boots;
+    [SerializeField] EquipmentSlot main_hand;
+    [SerializeField] EquipmentSlot off_hand;
+
+
+}
