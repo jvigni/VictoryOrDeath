5fda5320806ca95924cb93fb1957825c0af57785
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-10-26 20:15:59 -0300

    startmenu wip

---
+++ Assets/MENUS/StartMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    [SerializeField] Button deusVult;
+    [SerializeField] Button about;
+    [SerializeField] Button quit;
+    [SerializeField] GameObject startMenu;
+    [SerializeField] GameObject hero;
+
+    private void Start()
+    {
+        deusVult.onClick.AddListener(DeusVult);
+        about.onClick.AddListener(About);
+        quit.onClick.AddListener(Quit);
+    }
+
+    void DeusVult()
+    {
+        startMenu.SetActive(false);
+        hero.SetActive(true);
+    }
+
+    void About()
+    {
+        // TODO
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
+}
