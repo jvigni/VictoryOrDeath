900ea584f009358bbf55f62dacc80f154ad88095
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-11-07 15:30:01 -0300

    drag

--- Assets/MENUS/HERO_MENU/Spells/SpellSlot.cs
+++ Assets/MENUS/HERO_MENU/Spells/SpellSlot.cs
@@ -5,18 +5,29 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class SpellSlot : MonoBehaviour, IDropHandler
+public class SpellSlot : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
     public Image icon;
     public Ability ability;
 
-    public void OnDrop(PointerEventData eventData)
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        Debug.Log("ON DROP");
+        Debug.Log("OnBeginDrag called");
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnDrag called");
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("OnEndDrag called");
     }
 
     internal void Init(Ability ability)
     {
+        Debug.Log("Init method called");
         this.ability = ability;
         icon.sprite = ability.Icon;
     }
