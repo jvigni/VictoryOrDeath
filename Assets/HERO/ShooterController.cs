c55e0d73289feeaa4cafb575fa4b16d73dc7fe5a
Juan Vigni <56237750+jvigni@users.noreply.github.com>
2024-11-09 10:59:44 -0300

    asd

--- Assets/HERO/ShooterController.cs
+++ Assets/HERO/ShooterController.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class ShooterController : MonoBehaviour
 {
-    
+    [SerializeField] LayerMask mouseColliderLayerMask;
+    [SerializeField] Transform aimTrainsform;
+
+    private void Update()
+    {
+        /*
+        Vector2 screen2CenterPoint = new Vector2(Screen.width / 2f, Screen.height / 2f);
+        //Ray ray = Camera.main.ScreenPointToRay(screen2CenterPoint);
+        Ray ray = Camera.main.ViewportPointToRay(screen2CenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, mouseColliderLayerMask))
+        {   
+            aimTrainsform.position = raycastHit.point;
+        }*/
+    }
 }
